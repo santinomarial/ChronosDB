5d4c1ba1b760a3a4f780e06a926e5c3d433b180b
santinomarial <santinomakuei25@gmail.com>
2026-08-15 16:26:20 -0400

    test: avoid persistent log header collision

--- tests/raft/persistent_log_header_self_contained.cpp
+++ tests/raft/persistent_log_header_self_contained.cpp
@@ -1,5 +1,5 @@
 #include "chronos/raft/persistent_log.hpp"
 
-[[maybe_unused]] chronos::raft::RaftPersistentLog log;
+[[maybe_unused]] chronos::raft::RaftPersistentLog raft_log;
 
 static_assert(chronos::raft::kDefaultRaftSegmentTargetSize == std::uint64_t{64U} * 1024U * 1024U);
